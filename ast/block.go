708acf7fe0136555b0686a2fd358cf5eda1ab823
seailly <65954222+seailly@users.noreply.github.com>
2022-09-14 09:07:10 +0100

    Add a `BlockStatement` to ast

---
+++ ast/block.go
@@ -0,0 +1,29 @@
+package ast
+
+import (
+	"bytes"
+
+	"github.com/seailly/mi/token"
+)
+
+// BlockStatement
+type BlockStatement struct {
+	Token      token.Token
+	Statements []Statement
+}
+
+func (bs *BlockStatement) statementNode() {}
+
+func (bs *BlockStatement) TokenLiteral() string {
+	return bs.Token.Literal
+}
+
+func (bs *BlockStatement) String() string {
+	var out bytes.Buffer
+
+	for _, s := range bs.Statements {
+		out.WriteString(s.String())
+	}
+
+	return out.String()
+}
