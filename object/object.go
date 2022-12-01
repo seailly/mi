f642ad00e97faff9be82c24b15f4a2834062c62c
seailly <65954222+seailly@users.noreply.github.com>
2022-12-01 08:59:21 +0000

    feat(eval): Handle user input errors to eval

--- object/object.go
+++ object/object.go
@@ -3,10 +3,11 @@ package object
 type ObjectType string
 
 const (
-	INTEGER_OBJECT = "INTEGER"
-	BOOLEAN_OBJECT = "BOOLEAN"
-	NULL_OBJECT = "NULL"
+	INTEGER_OBJECT      = "INTEGER"
+	BOOLEAN_OBJECT      = "BOOLEAN"
+	NULL_OBJECT         = "NULL"
 	RETURN_VALUE_OBJECT = "RETURN_VALUE"
+	ERROR_OBJECT        = "ERROR"
 )
 
 // Object Each value represents itself
