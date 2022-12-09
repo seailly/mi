8bbb2c36493f294808cbcdd58687bfd3b9817134
seailly <65954222+seailly@users.noreply.github.com>
2022-12-09 23:47:50 +0000

    feat(eval): Create outer environment when evaluating function

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -88,6 +88,8 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 		if len(args) == 1 && isError(args[0]) {
 			return args[0]
 		}
+
+		return applyFunction(function, args)
 	}
 
 	return nil
@@ -240,7 +242,7 @@ func evalIdentifier(node *ast.Identifier, env *object.Environment) object.Object
 	return val
 }
 
-func evalExpression(
+func evalExpressions(
 	exps []ast.Expression,
 	env *object.Environment,
 ) []object.Object {
@@ -255,6 +257,40 @@ func evalExpression(
 	}
 
 	return result
+}
+
+// applyFunction Create a new outer environment when evaluating a function
+func applyFunction(fn object.Object, args []object.Object) object.Object {
+	function, ok := fn.(*object.Function)
+	if !ok {
+		return newError("not a function: %s", fn.Type())
+	}
+
+	extendedEnv := extendFunctionEnv(function, args)
+	evaluated := Eval(function.Body, extendedEnv)
+
+	return unwrapReturnValue(evaluated)
+}
+
+func extendFunctionEnv(
+	fn *object.Function,
+	args []object.Object,
+) *object.Environment {
+	env := object.NewEnclosedEnvironment(fn.Env)
+
+	for paramIdx, param := range fn.Parameters {
+		env.Set(param.Value, args[paramIdx])
+	}
+
+	return env
+}
+
+func unwrapReturnValue(obj object.Object) object.Object {
+	if returnValue, ok := obj.(*object.ReturnValue); ok {
+		return returnValue.Value
+	}
+
+	return obj
 }
 
 func isTruthy(obj object.Object) bool {
